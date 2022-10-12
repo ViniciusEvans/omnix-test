13e04d3da265b17f3d0281e70e94d20ffbe2ffa0
Vinicius <vinibrother.evansz2@gmail.com>
2022-10-11 21:40:02 -0300

    feat: add not found page

---
+++ src/components/page404.tsx
@@ -0,0 +1,19 @@
+export function Page404() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#2d2d2d",
+        color: "#fafbfc",
+        display: "flex",
+        flexDirection: "row",
+        gap: "0",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>404 - Page not found</h1>
+    </div>
+  );
+}
